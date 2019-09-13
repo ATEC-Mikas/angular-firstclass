d8ae6fc8626798f3eda96d336b76b0dd2ee8dadc
ATEC-Mikas <miguel.pereira.t0114411@edu.atec.pt>
2019-09-13 20:56:59 +0100

    2 files modified

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -7,21 +7,17 @@ import { Turma } from '../turma';
   styleUrls: ['./class.component.css']
 })
 export class ClassComponent implements OnInit {
-  // name:string;
-  // number_students:number;
-  // date_start:Date;
-
-  // constructor(name:string, number_students:number, date_start:Date) {
-  //   this.name = name;
-  //   this.number_students = number_students;
-  //   this.date_start = date_start;
-  // }
+  editable:boolean = false;
 
   turma:Turma = {
     name: "TPSI 10.18",
     description: "Uma turma",
     start_date: new Date(2018,10,22),
   };
+
+  enableEdit() {
+    this.editable = !this.editable;
+  }
 
   constructor() {
 
